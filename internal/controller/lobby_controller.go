7757655b674b87e7dc0a30b1c92b56bbb4eeac39
lester.wei <ly.lester@rayprosoft.com>
2023-12-22 10:59:16 +0800

    fix

--- internal/controller/lobby_controller.go
+++ internal/controller/lobby_controller.go
@@ -22,12 +22,15 @@ import (
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	appv1 "app-controller/api/v1"
+	"app-controller/internal/controller/utils"
 )
 
 // LobbyReconciler reconciles a Lobby object
@@ -65,40 +68,40 @@ func (r *LobbyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	// //根据app的配置进行处理
-	// //1. Deployment的处理
-	// deployment := utils.NewDeployment(app, logger)
-	// if err := controllerutil.SetControllerReference(app, deployment, r.Scheme); err != nil {
-	// 	return ctrl.Result{}, err
-	// }
-	// //查找同名deployment
-	// d := &v1.Deployment{}
-	// if err := r.Get(ctx, req.NamespacedName, d); err != nil {
-	// 	if errors.IsNotFound(err) {
-	// 		if err := r.Create(ctx, deployment); err != nil {
-	// 			logger.Error(err, "create deploy failed")
-	// 			return ctrl.Result{}, err
-	// 		}
-	// 	}
-	// } else {
-	// 	//Bug: 这里会反复触发更新
-	// 	//原因：在148行SetupWithManager方法中，监听了Deployment，所以只要更新Deployment就会触发
-	// 	//     此处更新和controllerManager更新Deployment都会触发更新事件，导致循环触发
-	// 	//修复方法：
-	// 	//方式1. 注释掉在148行SetupWithManager方法中对Deployment，Ingress，Service等的监听，该处的处理只是为了
-	// 	//      手动删除Deployment等后能够自动重建，但正常不会出现这种情况，是否需要根据情况而定
-	// 	//方式2. 加上判断条件，仅在app.Spec.Replicas != deployment.Spec.Replicas &&
-	// 	//      app.Spec.Image != deployment.Spec.Template.Spec.Containers[0].Image时才更新deployment
+	//根据app的配置进行处理
+	//1. Deployment的处理
+	deployment := utils.NewDeployment(app, logger)
+	if err := controllerutil.SetControllerReference(app, deployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	//查找同名deployment
+	d := &v1.Deployment{}
+	if err := r.Get(ctx, req.NamespacedName, d); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, deployment); err != nil {
+				logger.Error(err, "create deploy failed")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		//Bug: 这里会反复触发更新
+		//原因：在148行SetupWithManager方法中，监听了Deployment，所以只要更新Deployment就会触发
+		//     此处更新和controllerManager更新Deployment都会触发更新事件，导致循环触发
+		//修复方法：
+		//方式1. 注释掉在148行SetupWithManager方法中对Deployment，Ingress，Service等的监听，该处的处理只是为了
+		//      手动删除Deployment等后能够自动重建，但正常不会出现这种情况，是否需要根据情况而定
+		//方式2. 加上判断条件，仅在app.Spec.Replicas != deployment.Spec.Replicas &&
+		//      app.Spec.Image != deployment.Spec.Template.Spec.Containers[0].Image时才更新deployment
 
-	// 	if app.Spec.Replicas != int(*deployment.Spec.Replicas) &&
-	// 		app.Spec.Image != deployment.Spec.Template.Spec.Containers[0].Image {
+		if app.Spec.Replicas != int(*deployment.Spec.Replicas) &&
+			app.Spec.Image != deployment.Spec.Template.Spec.Containers[0].Image {
 
-	// 		if err := r.Update(ctx, deployment); err != nil {
-	// 			return ctrl.Result{}, err
+			if err := r.Update(ctx, deployment); err != nil {
+				return ctrl.Result{}, err
 
-	// 		}
-	// 	}
-	// }
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
